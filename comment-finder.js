5bc25ccdc3d82f5eadfe73600f56d03ce3484b2f
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-11-02 16:13:24 -0700

    comment finder script working except for first few comments MUST FIX!

--- comment-finder.js
+++ comment-finder.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 (async () => {
@@ -14,26 +14,39 @@ const rl = readline.createInterface({
     await page.goto(postURL);
 
     rl.question('Enter the keyword you want to search for in comments: ', async (keyword) => {
-      // Scroll through the comment section to load comments (if necessary).
-      // You can use the scrolling code mentioned earlier.
+      let found = false;
 
-      // Wait for the main container with class "_ae65" to appear.
-      await page.waitForSelector('._ae65'); // Make sure this selector is correct.
+      while (true) {
+        const commentTextElements = await page.$$(
+          'span._aacl._aaco._aacu._aacx._aad7._aade'
+        );
 
-      // Traverse the DOM to reach the h1 element with class "_aac1" containing the text.
-      const commentText = await page.evaluate(() => {
-        const commentContainer = document.querySelector('._ae65 .x9f619 ._ae1k ._ae2s div:nth-child(3) ._ae5q ul ._a9z6 .x1qjc9v5 ._a9zj ._a9zm ._a9zn ._a9zr ._a9zc ._a9zs ._aac1');
-        return commentContainer ? commentContainer.textContent : null;
-      });
+        for (const element of commentTextElements) {
+          const commentText = await element.evaluate((el) => el.textContent);
 
-      if (commentText) {
-        if (commentText.includes(keyword)) {
-          console.log(`Comment text: ${commentText}`);
-        } else {
-          console.log(`The comment text does not contain the keyword: ${keyword}`);
+          if (commentText.includes(keyword)) {
+            console.log(`Comment text: ${commentText}`);
+            found = true;
+          }
         }
-      } else {
-        console.log('Comment section or the target element not found on the page.');
+
+        // Scroll down to load more comments.
+        const comments = await page.$('.Mr508');
+        if (comments) {
+          await comments.evaluate(() => {
+            const comments = document.querySelector('.Mr508');
+            comments.scrollTop = comments.scrollHeight;
+          });
+        } else {
+          break;
+        }
+
+        // Wait for some time after scrolling.
+        await new Promise(resolve => setTimeout(resolve, 2000)); // Adjust the delay as needed.
+      }
+
+      if (!found) {
+        console.log(`The keyword "${keyword}" was not found in any comment.`);
       }
 
       await browser.close();
